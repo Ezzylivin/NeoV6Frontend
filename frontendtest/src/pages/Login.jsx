d84187a84187956869d603621f94df80d9154ebf
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-05 02:19:50 -0400

    Update Login.jsx

--- frontendtest/src/pages/Login.jsx
+++ frontendtest/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { AuthProvider } from './hooks/useAuth';
+import { AuthProvider } from './hooks/useAuth.jsx';
 import { loginUser } from '../api/auth.js';
 import { useNavigate } from 'react-router-dom';
 
