9710121ba3ac86299ba3a93181805a731e440323
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-05 00:54:53 -0400

    Update App.jsx

--- frontendtest/src/App.jsx
+++ frontendtest/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import { AuthProvider } from './context/AuthContext.jsx';
+import { AuthProvider } from './hooks/useAuth';
 import PrivateRoute from './components/PrivateRoute.jsx';
 
 import Login from './pages/Login.jsx';
