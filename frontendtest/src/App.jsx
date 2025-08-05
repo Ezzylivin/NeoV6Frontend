e30df0114856f020eb6ffdee831b60bafde9f8d1
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-05 10:18:00 -0400

    Update App.jsx

--- frontendtest/src/App.jsx
+++ frontendtest/src/App.jsx
@@ -4,12 +4,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './hooks/useAuth';
 import PrivateRoute from './components/PrivateRoute.jsx';
 
+import './api/logs';
+
 import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
 import Dashboard from './pages/Dashboard.jsx';
 import Backtests from './pages/Backtests.jsx';
 import UserSettings from './pages/UserSettings.jsx';
 import BotTraining from './pages/BotTraining.jsx';
+import NotFound from './pages/NotFound.jsx';
 
 
 export default function App() {
