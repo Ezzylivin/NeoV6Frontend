78f154daf00450e7fa65fc2d2b62bf96e7a88777
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-05 02:25:15 -0400

    Update Dashboard.jsx

--- frontendtest/src/pages/Dashboard.jsx
+++ frontendtest/src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { AuthProvider } from './hooks/useAuth.jsx';
+import { AuthProvider } from '../hooks/useAuth.jsx';
 import { startBot, stopBot, getBotStatus } from '../api/bot.js';
 import { fetchLogs } from '../api/logs.js';
 import NavBar from '../components/NavBar.jsx';
