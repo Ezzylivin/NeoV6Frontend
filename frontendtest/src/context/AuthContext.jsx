416e2d9dbf0c32bc0926adcf51f13ecf641b15c9
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-06 02:34:14 -0400

    Update AuthContext.jsx

--- frontendtest/src/context/AuthContext.jsx
+++ frontendtest/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React, { createContext, useContext, useEffect, useState } from 'react';
 import { loginUser, registerUser } from '../api/auth';
 import { setAuthToken } from '../api/apiClient';
 
-export const AuthContext = createContext();
+export const Auth = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(() => localStorage.getItem('token') || null);
