1ed262c82dfeb427e4464817bd18a311c103adc3
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-06 02:13:03 -0400

    Update Backtests.jsx

--- frontendtest/src/pages/Backtests.jsx
+++ frontendtest/src/pages/Backtests.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { useAuth } from '../context/AuthContext.jsx';
+import { AuthContext } from '../context/AuthContext.jsx';
 import NavBar from '../components/NavBar.jsx';
 
 const Backtests = () => {
-  const { token } = useAuth();
+  const { token } = useContext(AuthContext);
   const [results, setResults] = useState([]);
   const [timeframeFilter, setTimeframeFilter] = useState('');
   const [loading, setLoading] = useState(false);
