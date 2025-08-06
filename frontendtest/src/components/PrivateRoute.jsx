28df3711a2255ca78779290ef65206ff93c908db
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-06 02:35:51 -0400

    Update PrivateRoute.jsx

--- frontendtest/src/components/PrivateRoute.jsx
+++ frontendtest/src/components/PrivateRoute.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
-import AuthContext from '../context/AuthContext.jsx';
+import Auth from '../context/AuthContext.jsx';
 const PrivateRoute = ({ children, roles = [] }) => {
   const { user } = useContext(AuthContext);
 
