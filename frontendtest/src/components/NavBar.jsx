d6ec62c725156ef683e49817797c3ae16cde7090
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-06 02:08:40 -0400

    Update NavBar.jsx

--- frontendtest/src/components/NavBar.jsx
+++ frontendtest/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import AuthContext from '../context/AuthContext.jsx';
 const NavBar = () => {
-  const { user, logout } = useContext(AuthProvider);
+  const { user, logout } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleLogout = () => {
