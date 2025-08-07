08425309e5ab92865b1db928f518252ed6539535
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-07 11:02:39 -0400

    Update main.jsx

--- frontendtest/src/main.jsx
+++ frontendtest/src/main.jsx
@@ -12,7 +12,12 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-   <App />
-  </React.StrictMode>
+ <React.StrictMode>
+  <AuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthProvider>
+</React.StrictMode>
+
 );
