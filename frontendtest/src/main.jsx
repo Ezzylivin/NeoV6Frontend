3d50e496a9036be7bbf05f6bdcf03975ef012a1a
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-08 13:48:45 -0400

    Update main.jsx

--- frontendtest/src/main.jsx
+++ frontendtest/src/main.jsx
@@ -2,8 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthProvider.jsx';
 
-import App from './App.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
