6f69c263c275d6fb57bf2ce387a5aef84b78ac1a
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-08 22:51:38 -0400

    Update main.jsx

--- frontendtest/src/main.jsx
+++ frontendtest/src/main.jsx
@@ -1,17 +1,16 @@
-// File: src/main.jsx
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthProvider.jsx';
 
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <BrowserRouter>
+    <BrowserRouter>
+      <AuthProvider>
         <App />
-      </BrowserRouter>
-    </AuthProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
