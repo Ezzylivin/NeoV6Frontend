1cd3b0167855f821f4c4c3f106919343d1bf2e7b
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-08 22:47:29 -0400

    Update main.jsx

--- frontendtest/src/main.jsx
+++ frontendtest/src/main.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthProvider.jsx';
-import './index.css';
 
 
 
