b33bdc5b77c9f5e7081f109c51fc6cec46bd17a1
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-09 21:09:03 -0400

    Update Dashboard.jsx

--- frontendtest/src/pages/Dashboard.jsx
+++ frontendtest/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDashboard } from '../hooks/useDashboard.jsx';
-import { useAuth } from '../context/AuthContext.jsx';
+import { useAuth } from '../contexts/AuthContext.jsx';
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
   const { user } = useAuth(); // Get logged-in user
