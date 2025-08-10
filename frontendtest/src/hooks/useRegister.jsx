e83804601e3307a34d9ef0ea2e5e04fa7fff2646
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-10 00:48:37 -0400

    Update useRegister.jsx

--- frontendtest/src/hooks/useRegister.jsx
+++ frontendtest/src/hooks/useRegister.jsx
@@ -1,6 +1,6 @@
 // src/hooks/useRegister.js
 import { useState } from "react";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../context/AuthContext.jsx";
 import apiClient from "../api/apiClient";
 
 export const useRegister = () => {
