5408120c3dda9c573615fb3ca53366ac15fb8c80
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-11 04:59:02 -0400

    Update auth.js

--- frontendtest/src/api/auth.js
+++ frontendtest/src/api/auth.js
@@ -14,7 +14,7 @@ export const registerUser = async (username,email, password) => {
 // Log in an existing user
 export const loginUser = async (email, password) => {
   try {
-    const response = await apiClient.get('/user/login', { email, password });
+    const response = await apiClient.post('/user/login', { email, password });
     return response.data;
   } catch (error) {
     throw new Error(error.response?.data?.message || 'Login failed');
