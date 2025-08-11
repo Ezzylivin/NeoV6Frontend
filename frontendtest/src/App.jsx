e9ab06872209d963d0d9aa86344a07b5ce638829
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-11 05:12:06 -0400

    Update App.jsx

--- frontendtest/src/App.jsx
+++ frontendtest/src/App.jsx
@@ -34,7 +34,7 @@ export default function App() {
         <Routes>
           {/* --- GUEST-ONLY ROUTES --- */}
           {/* These routes are for users who are NOT logged in. */}
-          <Route path="/" element={
+          <Route path="/auth" element={
             <GuestRoute>
               <AuthPage />
             </GuestRoute>
