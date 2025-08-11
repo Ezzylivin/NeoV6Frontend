0eb794885f4e69cbac4d2c4ddde9e0cf93332761
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-11 18:38:06 -0400

    Update apiClient.js

--- frontendtest/src/api/apiClient.js
+++ frontendtest/src/api/apiClient.js
@@ -1,18 +1,19 @@
 import axios from 'axios';
+import { API_BASE_URL } from "../components/config.js
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  
+  baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   }
 });
 
-export const setAuthToken = token => {
-  if (token) {
-    apiClient.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-  } else {
-    delete apiClient.defaults.headers.common['Authorization'];
+api.interceptors.request.use(config => {
+  const generateToken = localStorage.getItem("token");
+  if (token)n {
+    config.headers.Authorization = 'Bearer ${token}';
   }
-};
+  return config;
 
 export default apiClient;
