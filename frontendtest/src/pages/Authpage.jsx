fc2760132884f8e3eb83358c336e9aaf1276eaaa
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-12 13:18:47 -0400

    Update Authpage.jsx

--- frontendtest/src/pages/Authpage.jsx
+++ frontendtest/src/pages/Authpage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useAuth } from '../contexts/AuthContext.jsx'; 
+import { useAuth } from '../context/AuthContext.jsx'; 
 import { useNavigate } from 'react-router-dom';
 
 export default function AuthPage() {
