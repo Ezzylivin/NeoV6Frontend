d58914ecfeeddc1f31c0ec799f1d464365843858
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-22 11:21:45 -0400

    Update useRegister.js

--- frontendtest/src/hooks/useRegister.js
+++ frontendtest/src/hooks/useRegister.js
@@ -13,7 +13,7 @@ export const useRegister = (user._id) => {
       setLoading(true);
       setError(null);
 
-      const { data } = await apiClient.post("/auth/user/register", formData);
+      const { data } = await apiClient.post("/users/register", formData);
       // data = { token, user }
 
       // ✅ Auto-login immediately after successful register
