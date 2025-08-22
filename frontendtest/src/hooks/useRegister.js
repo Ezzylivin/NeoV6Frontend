e81b43b9a13ccfc016c8c03621d4407b928d6c22
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-22 11:01:48 -0400

    Create useRegister.js

---
+++ frontendtest/src/hooks/useRegister.js
@@ -0,0 +1,32 @@
+// src/hooks/useRegister.js
+import { useState } from "react";
+import { useAuth } from "../context/AuthContext.jsx";
+import apiClient from "../api/apiClient";
+
+export const useRegister = (user._id) => {
+  const { login } = useAuth(); // same as in useLogin
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const registerUser = async (formData) => {
+    try {
+      setLoading(true);
+      setError(null);
+
+      const { data } = await apiClient.post("/auth/user/register", formData);
+      // data = { token, user }
+
+      // ✅ Auto-login immediately after successful register
+      login(data.user, data.token);
+
+      return { success: true };
+    } catch (err) {
+      setError(err.response?.data?.message || "Registration failed");
+      return { success: false };
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { registerUser, loading, error };
+};
