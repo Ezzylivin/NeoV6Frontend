05fd1a78b31f022fde715eb94a365eb5922ebcc9
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-27 23:07:48 -0400

    Update DashboardLayout.jsx

--- frontendtest/src/layouts/DashboardLayout.jsx
+++ frontendtest/src/layouts/DashboardLayout.jsx
@@ -3,10 +3,15 @@
 import React from 'react';
 // Outlet is a special component from react-router-dom that renders the child route's element
 import { Outlet } from 'react-router-dom';
+import NavBar from '../components/NavBar';
 import Header from '../components/Header.jsx';
 
 const DashboardLayout = () => {
   return (
+        <div className="min-h-screen flex flex-col">
+      {/* NavBar MUST be rendered here */}
+      <NavBar />
+
     <div className="dashboard-layout">
       {/* The Header will be rendered at the top of every page using this layout */}
       <Header />
