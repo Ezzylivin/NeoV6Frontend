58ee6a65420c30ce5ee605dcab2d8e22a5014e0a
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-28 18:51:19 -0400

    Update DashboardLayout.jsx

--- frontendtest/src/layouts/DashboardLayout.jsx
+++ frontendtest/src/layouts/DashboardLayout.jsx
@@ -1,17 +1,17 @@
-// File: src/layouts/DashboardLayout.jsx
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import NavBar from '../components/NavBar.jsx';
 
 const DashboardLayout = () => {
   return (
-    <div className="min-h-screen flex flex-col">
-      {/* NavBar at the top */}
+    <div className="min-h-screen flex flex-col bg-black text-white">
       <NavBar />
 
-      {/* Main content */}
-      <main className="flex-1 p-6 bg-gray-100 text-gray-900">
-        <Outlet />
+      <main className="flex-1 p-6 max-w-7xl mx-auto w-full">
+        {/* Card-like container for page content */}
+        <div className="bg-gray-900 rounded-xl shadow-lg p-6">
+          <Outlet />
+        </div>
       </main>
     </div>
   );
