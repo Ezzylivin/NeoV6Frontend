45844a45dee75629526fd1991ee16c6d4fe3fd4f
Ezzylivin <167244051+Ezzylivin@users.noreply.github.com>
2025-08-29 00:00:32 -0400

    Update DashboardLayout.jsx

--- frontendtest/src/layouts/DashboardLayout.jsx
+++ frontendtest/src/layouts/DashboardLayout.jsx
@@ -1,13 +1,19 @@
 // File: src/layouts/DashboardLayout.jsx
 import React from "react";
 import { Outlet } from "react-router-dom";
+import NavBar from "../components/NavBar.jsx"; // import NavBar
 
 export default function DashboardLayout() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-black text-white">
+    <div className="flex flex-col min-h-screen bg-black text-white">
+      {/* Top navigation bar */}
+      <NavBar />
+
       {/* Main content wrapper */}
-      <div className="w-full max-w-4xl p-6">
-        <Outlet />
+      <div className="flex flex-1 justify-center items-center">
+        <div className="w-full max-w-4xl p-6">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
